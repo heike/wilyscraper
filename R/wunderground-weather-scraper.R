b562b8a3d783d7473b028d14be6c6473ffb6498f
Susan Vanderplas <srvanderplas+github@gmail.com>
2018-08-29 17:03:52 -0500

    weather underground scraper with selenium.

--- R/wunderground-weather-scraper.R
+++ R/wunderground-weather-scraper.R
@@ -1,5 +1,5 @@
 #' Function to get weather data from weather underground
-#' 
+#'
 #' @param remDr a selenium webdriver client
 #' @param station station call sign
 #' @param year year (4 digit number)
@@ -7,76 +7,90 @@
 #' @param day day (1 or 2 digit number)
 #' @return table of weather data, with date, time, variable, value, and unit
 #' @import rvest
-#' @import stringr
-#' @import dplyr
+#' @importFrom stringr str_extract str_trim str_replace_all
+#' @importFrom dplyr '%>%'
 #' @importFrom tidyr gather
-getWeather_day <- function(remDr, station = "KAMW", year = 2015, month = 1, day = 1) {
+wUnderground_day <- function(remDr, station = "KAMW", year = 2015, month = 1, day = 1) {
   numbers <- "[-]*[0-9\\.]*[e0-9]*"
   units <- "[°a-zA-Z\\s\\\\]+"
 
   url <-
     sprintf(
-      "https://www.wunderground.com/history/airport/%s/%04d/%02d/%02d/DailyHistory.html", 
+      "https://www.wunderground.com/history/airport/%s/%04d/%02d/%02d/DailyHistory.html",
       station, year, month, day
     )
 
   remDr$open()
+  on.exit(remDr$close())
   # Establish a wait for an element
   remDr$setImplicitWaitTimeout(1000)
   remDr$navigate(url)
-  Sys.sleep(5)
-  html <- remDr$getPageSource()[[1]] %>% read_html()
-  remDr$close()
-  tabs <- html %>% html_nodes("#history-observation-table")
+  i <- 0
+  tabs <- NULL
+  while (i < 3 & length(tabs) == 0) {
+    Sys.sleep(2)
+    html <- remDr$getPageSource()[[1]] %>% read_html()
+    tabs <- html %>% html_nodes("#history-observation-table")
+  }
   if (length(tabs) > 0) {
     day.stats <- html_table(tabs[[length(tabs)]], fill = TRUE)
-    
+
     day.stats.long <- day.stats %>% tidyr::gather(key = Var, value = Value, -Time)
     if (is.null(day.stats.long$Value)) return(NULL) # no data available
-    
+
     day.stats.long$Unit <- str_extract(day.stats.long$Value, pattern = units) %>%
-      str_replace_all("\\s{1,}", " ") %>% str_trim()
+      str_replace_all("\\s{1,}", " ") %>%
+      str_trim()
     day.stats.long$Value <- as.numeric(str_extract(day.stats.long$Value, pattern = numbers))
     day.stats.long$Station <- station
     day.stats.long$Date <- sprintf("%s-%s-%s", year, month, day)
-    
+
     return(day.stats.long)
   } else {
     return(NULL)
   }
-
 }
 
 #' Function to get all weather data from wunderground for a period of time
-#' 
+#'
 #' @param start start date
 #' @param end end date
 #' @param station weather station
 #' @param file file to save the CSV data file
 #' @import RSelenium
-#' @import lubridate
-#' @import dplyr
-getWeather_period <- function(
-  start = lubridate::ymd("2009-01-01"), 
-  end = lubridate::today(), 
-  station = "KAMW", 
-  file = file.path("data", paste0(station, "_weather.csv"))) {
+#' @importFrom lubridate today ymd year month mday
+#' @importFrom dplyr '%>%'
+#' @export
+wUnderground_data <- function(start = lubridate::ymd("2011-01-01"),
+                              end = lubridate::today(),
+                              station = "KAMW") {
   days <- seq(start, end, "day")
-  
+
+  # crx <- tempfile(fileext = ".zip")
+  # crxfolder <- tempdir()
+  # download.file("https://github.com/gorhill/uBlock/releases/download/1.16.21b1/uBlock0.chromium.zip",
+  #               destfile = crx)
+  # unzip(crx, exdir = crxfolder)
+  # list.files(file.path(crxfolder), full.names = T)
   # Start selenium driver
   rD <- RSelenium::rsDriver(browser = "chrome", version = "3.13.0")
   remDr <- rD[["client"]]
-  
-  res <- purrr::map(
-    days, 
+
+  res <- purrr::map_dfr(
+    days,
     ~try(
-      getWeather_day(
-        remDr = remDr, station = station, 
-        lubridate::year(.), lubridate::month(.), lubridate::mday(.)))
+      wUnderground_day(
+        remDr = remDr, station = station,
+        lubridate::year(.), lubridate::month(.), lubridate::mday(.)
+      )
+    )
   )
+
+  # write.table(res,
+  #   file = file, append = file.exists(file),
+  #   col.names = !file.exists(file),
+  #   row.names = F, sep = ","
+  # )
   
-  
-  write.table(res, file=file, append=file.exists(file),
-              col.names=!file.exists(file),
-              row.names=F, sep=",")
+  return(res)
 }
